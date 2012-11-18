fa59093882b68e1f0122af7b4ed052a952c540cc
Femaref <femaref@googlemail.com>
2012-11-18 17:14:20 +0100

    version bump

--- lib/checkup/version.rb
+++ lib/checkup/version.rb
@@ -13,7 +13,7 @@ module Checkup
     #  Defines the minor version
     # PATCH:
     #  Defines the patch version
-    MAJOR, MINOR, PATCH = 0, 0, 4
+    MAJOR, MINOR, PATCH = 0, 0, 5
 
     ##
     # Returns the major version ( big release based off of multiple minor releases )
